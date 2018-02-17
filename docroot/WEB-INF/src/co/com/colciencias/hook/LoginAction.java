ce57668b0eac77e397f1439fdf01b40cfa090a5b
YumeG <camisimpson19@gmail.com>
2018-02-17 16:49:40 -0500

    adicion de send redirect

--- docroot/WEB-INF/src/co/com/colciencias/hook/LoginAction.java
+++ docroot/WEB-INF/src/co/com/colciencias/hook/LoginAction.java
@@ -1,5 +1,7 @@
 package co.com.colciencias.hook;
 
+import java.io.IOException;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -8,5 +10,11 @@ import com.liferay.portal.kernel.events.Action;
 public class LoginAction extends Action {
     public void run(HttpServletRequest req, HttpServletResponse res) {
         System.out.println("## My custom login action");
+        try {
+			res.sendRedirect("/group/guest/resumen-grupo");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
 }
